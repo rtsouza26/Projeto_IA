c0d3e64fe0b59df404c42cf579e2042f33cb5ebe
Daniel <daniellemosmelo@hotmail.com>
2019-11-02 17:52:47 -0300

    Normalizando os dados e executando

--- Exec_All.py
+++ Exec_All.py
@@ -1,9 +1,10 @@
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
-#from sklearn.neighbors import KNeighborsClassifier
+from sklearn import preprocessing
 import numpy as np
 import pandas as pd
 from HCV import knn
+
 
 
 def accuracy(y_true, y_pred):
@@ -17,13 +18,19 @@ def accuracy(y_true, y_pred):
 
 
 hcv = pd.read_csv('HCV-Egy-Data.csv')
-
 labels = np.array(hcv['Baselinehistological staging'])
 hcv = hcv.drop('Baselinehistological staging', axis = 1)
-hcv_list = list(hcv.columns)
-hcv = np.array(hcv)
-
-train_features, test_features, train_labels, test_labels = train_test_split(hcv, labels, test_size = 0.25, random_state = 42)
+columns = hcv.columns
+repcolumns = list()
+for index, column in enumerate(columns):
+  column = column.replace(" ","")
+  repcolumns.append(column)
+y = hcv.values
+min_max_scaler = preprocessing.MinMaxScaler()
+x_scaled = min_max_scaler.fit_transform(y)
+dfnorm = pd.DataFrame(x_scaled, columns = repcolumns)
+dfnorm = np.array(dfnorm)
+train_features, test_features, train_labels, test_labels = train_test_split(dfnorm, labels, test_size = 0.25, random_state = 42)
 n_arvores = [100,200,400]#testando com as profundidades variando entre 100,200 e 400
 print('Random Forest:')
 for x in n_arvores:
@@ -36,9 +43,9 @@ for x in n_arvores:
 print('KNN:')
 n_vizinhos = [1,3,5,10]
 for x in n_vizinhos:
-   kn = knn(k=x)
-   kn.train(train_features,train_labels)
-   predictions = kn.predict(test_features)
-   porcentagem = accuracy(test_labels, predictions)
-   print(porcentagem,'para',x,'vizinhos')
+    kn = knn(k=x)
+    kn.train(train_features,train_labels)
+    predictions = kn.predict(test_features)
+    porcentagem = accuracy(test_labels, predictions)
+    print(porcentagem,'para',x,'vizinhos')
 
